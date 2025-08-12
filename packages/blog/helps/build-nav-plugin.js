dcd15ca2dcc55abd19facb4f47687cd5c73f9041
橘子哥哥 <q295563358@gmail.com>
2025-08-12 15:39:12 +0800

    chore: 更新文件夹名字 documents -> posts

--- packages/blog/helps/build-nav-plugin.js
+++ packages/blog/helps/build-nav-plugin.js
@@ -1,60 +1,58 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path')
-const fsPromises = require('fs/promises')
+const path = require("path");
+const fsPromises = require("fs/promises");
 
-const argv = process.argv
-const dev = 'dev'
+const argv = process.argv;
+const dev = "dev";
 
-const documentsPath = () => path.resolve(__dirname, '../documents')
-const outPutBasePath = () => path.resolve(__dirname, '../.vitepress/router')
+const documentsPath = () => path.resolve(__dirname, "../posts");
+const outPutBasePath = () => path.resolve(__dirname, "../.vitepress/router");
 
-const mdFilePath = '/JSCore'
-const excludeDir = 'temp'
+const mdFilePath = "/JSCore";
+const excludeDir = "temp";
 
 const getComponentsSideBar = async () => {
-  const resolvePath = documentsPath() 
+  const resolvePath = documentsPath();
 
-  let dirArr = await fsPromises.readdir(resolvePath)
+  let dirArr = await fsPromises.readdir(resolvePath);
 
   if (argv[2] !== dev) {
-    dirArr = dirArr.filter((item) => item !== excludeDir)
+    dirArr = dirArr.filter((item) => item !== excludeDir);
   }
 
   return Promise.all(
     dirArr.map(async (dirItemPath) => {
-      const dirPath = `${resolvePath}/${dirItemPath}`
-      const fileArr = await fsPromises.readdir(dirPath)
+      const dirPath = `${resolvePath}/${dirItemPath}`;
+      const fileArr = await fsPromises.readdir(dirPath);
       return {
         text: dirItemPath,
         collapsible: true,
         collapsed: false,
         items: await Promise.all(
           fileArr.map(async (fileName) => {
-          const sideName = fileName.split('.')[0]
+            const sideName = fileName.split(".")[0];
             return {
               text: sideName,
-              link: `/documents/${dirItemPath}/${
-                fileName.split('.')[0]
-              }`
-            }
+              link: `/posts/${dirItemPath}/${fileName.split(".")[0]}`,
+            };
           })
-        )
-      }
+        ),
+      };
     })
-  )
-}
+  );
+};
 
 async function init() {
-  const sideBarArr = await getComponentsSideBar(mdFilePath)
-  const outPutFile = `${outPutBasePath()}/documentsRouter.json`
-  const outPutDir = path.dirname(outPutFile)
-  await fsPromises.mkdir(outPutDir, { recursive: true })
+  const sideBarArr = await getComponentsSideBar(mdFilePath);
+  const outPutFile = `${outPutBasePath()}/documentsRouter.json`;
+  const outPutDir = path.dirname(outPutFile);
+  await fsPromises.mkdir(outPutDir, { recursive: true });
 
   await fsPromises.writeFile(outPutFile, JSON.stringify(sideBarArr), {
-    encoding: 'utf-8'
-  })
+    encoding: "utf-8",
+  });
 }
-init()
+init();
 
 // TODO: buildStart这个钩子会出现无限重启服务器的问题
 // export default function sideBarPlugin() {
