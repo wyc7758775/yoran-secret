3f5bc754aa1205eb32eb3c603917ae36b97c2660
橘子哥哥 <q295563358@gmail.com>
2025-08-20 19:30:06 +0800

    feat: 全局替换图片来源,使用图床;按需引入

--- packages/blog/.vitepress/theme/index.ts
+++ packages/blog/.vitepress/theme/index.ts
@@ -1,10 +1,29 @@
+import { App } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import 'element-plus/dist/index.css'
+// 只导入实际使用的组件
+import { ElImage, ElIcon, ElDialog, vLoading } from 'element-plus'
+// 只导入实际使用的图标
+import { ArrowLeft } from '@element-plus/icons-vue'
 import './main.css'
+
+const useElementPlusComps = (app: App) => {
+  // 注册实际使用的组件
+  app.use(ElImage)
+  app.use(ElIcon)
+  app.use(ElDialog)
+  // 注册v-loading指令
+  app.use(vLoading.directive)
+  // 注册v-loading指令的组件
+  app.component('v-loading', vLoading)
+  // 注册实际使用的图标
+  app.component('ArrowLeft', ArrowLeft)
+}
 
 export default {
   ...DefaultTheme,
   async enhanceApp(ctx: any) {
     DefaultTheme.enhanceApp(ctx)
+    useElementPlusComps(ctx.app)
   }
 }
